b27da6f3fcc3c90e4a9275df11600caed06adbcd
Nestor Leiva <netto14cr@gmail.com>
2024-02-29 13:37:53 -0600

    Add csv and excel tables

--- app.py
+++ app.py
@@ -11,21 +11,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('main/index.html')
 
 
 @app.route('/sentiment')
 def sentiment_analysis():
     # Renderizar la plantilla sin los comentarios
-    return render_template('review.html')
+    return render_template('sentiment/review.html')
 
 @app.route('/get_comments')
 def get_comments():
     # Name of the file to read
-    csv_name = 'amazon_reviews.csv'
+    csv_name = 'Amazon Reviews.csv'
     
     # Get the path to the CSV file
-    csv_file = os.path.join(app.root_path, 'static', csv_name)
+    csv_file = os.path.join(app.root_path, 'static', "files/"+csv_name)
 
     # Create a Sentiment object
     s = sentiment.Sentiment(csv_file)
@@ -40,8 +40,8 @@ def get_comments():
     return jsonify(analyzed_reviews)
 
 
-@app.route('/read_excel')
-def read_excel():
+@app.route('/read_csv')
+def read_csv():
     # Name of the file to read
     csv_employee = 'Employee Sample Data.csv'
     # Get the path to the CSV file
@@ -54,10 +54,24 @@ def read_excel():
         data = pd.read_csv(f, encoding=result['encoding'])
 
     # Renderizar la plantilla con los datos del archivo CSV
-    return render_template('read_information.html', data=data.to_dict(orient='records'))
+    return render_template('csv/csv_table.html', data=data.to_dict(orient='records'))
 
 
-
+@app.route('/read_excel')
+def read_excel():
+    
+    # Name of the file to read
+    excel_employee = 'Employee Sample Data.xlsx'
+    # Get the path to the Excel file
+    excel_employee_file = os.path.join(app.root_path, 'static', "files/"+excel_employee)
+    # Read the Excel file
+    data = pd.read_excel(excel_employee_file)
+    
+    # Replace 'NaT' in 'Exit Date' with 'Active'
+    data['Exit Date'] = data['Exit Date'].fillna('Active')
+    
+    # Render the template with the data from the Excel file
+    return render_template('excel/excel_table.html', data=data.to_dict(orient='records'))
 
 
 if __name__ == '__main__':
